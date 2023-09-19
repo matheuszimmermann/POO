07d814223e6eb8c44ccdc628061bcb4040503910
Matheus Zimmermann <125280019+matheuszimmermann@users.noreply.github.com>
2023-09-19 08:32:05 -0300

    Update Main.java

--- Atividades/Atv01_3Bi/Main.java
+++ Atividades/Atv01_3Bi/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        // teste dos metodos
+        // teste dos metodo cachorro
         Cachorros cachorro = new Cachorros("Pastor Alemao", "Bob", 6);
         cachorro.emitirSom();
         cachorro.mover();
